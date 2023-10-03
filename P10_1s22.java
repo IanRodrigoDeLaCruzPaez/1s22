863e67d5d65f376094ad7ba6376e9f6649d9e772
Ian Rodrigo De La Cruz Paez <144197726+IanRodrigoDeLaCruzPaez@users.noreply.github.com>
2023-10-03 17:41:31 -0600

    Delete P10_1s22.java
    borrar archivo

--- P10_1s22.java
+++
@@ -1,9 +0,0 @@
-public class P10_1s22{
-    public static void main(String[] args) {
-    if(true){
-    System.out.println("por que aparezco...");
-
-     }
-    System.out.println("que paso...");
-    }
- }
